bd9298f69080843eca3b17b0aaea2bb437f4fb31
Sanju <sanjupipal@gmail.com>
2021-02-10 12:12:09 +0530

    client theme setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+const App = () => (
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+    </Fragment>
+  </Router>
+);
+
+export default App;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ connectDB();
 
 //init middleware
 app.use(express.json({ extended: false }));
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));

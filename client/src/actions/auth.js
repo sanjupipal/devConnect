a94135c492dc92eb65c4a2558f2562bbcba1dfa3
Sanju <sanjupipal@gmail.com>
2021-02-15 15:29:18 +0530

    Dashboard Setup

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,7 +7,8 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  LOGOUT
+  LOGOUT,
+  CLEAR_PROFILE,
 } from "../actions/types";
 import setAuthToken from "../utils/setAuthToken";
 // Load User
@@ -83,6 +84,7 @@ export const login = (email, password) => async (dispatch) => {
 };
 
 //Logout
-export const logout = () => dispatch =>{
-  dispatch({type:LOGOUT})
-}
+export const logout = () => (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
+  dispatch({ type: LOGOUT });
+};
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,13 +1,38 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profile";
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+import Spinner from "../layout/spinner";
+import { Link } from "react-router-dom";
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading },
+}) => {
   useEffect(() => {
     getCurrentProfile();
   }, []);
 
-  return <div>Dashboard</div>;
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome {user && user.name}
+      </p>
+      {profile !== null ? (
+        <Fragment></Fragment>
+      ) : (
+        <Fragment>
+          <p>You have not setup a profile yet. Please add some info</p>
+          <Link to="/create-profile" className="btn btn-primary my-1">
+            Create Profile
+          </Link>
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 Dashboard.propTypes = {
